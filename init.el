d2c2208bc77b23f89e61e64e49fc157ec8731b82
shuwan <shuwan@emacsos>
2020-04-10 12:56:39 -0400

    CCInitial commit

---
+++ init.el
@@ -0,0 +1,37 @@
+;; MELPA setup for packages DO NOT REMOVE
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+  ;; and `package-pinned-packages`. Most users will not need or want to do this.
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  )
+(package-initialize)
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+;;link for org config
+(org-babel-load-file (expand-file-name "~/.emacs.d/my-init.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elpy-rpc-backend "jedi" t)
+ '(org-agenda-files (list org-directory))
+ '(org-directory "~/Documents/org")
+ '(package-selected-packages
+   (quote
+    (cloc steam shell-pop expand-region darkroom tldr which-key use-package smartparens ox-hugo org-bullets magit lua-mode flycheck exwm ess elpy doom-themes doom-modeline dashboard counsel-projectile company-quickhelp company-jedi))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
