e018230f05dec934efd496e20a79cb76e94b3355
shuwan4 <57843522+shuwan4@users.noreply.github.com>
2020-06-27 22:27:36 -0400

    update
    updated config files

--- init.el
+++ init.el
@@ -1,19 +1,5 @@
-;; MELPA setup for packages DO NOT REMOVE
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-  ;; and `package-pinned-packages`. Most users will not need or want to do this.
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  )
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 ;;link for org config
@@ -23,12 +9,11 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elpy-rpc-backend "jedi" t)
  '(org-agenda-files (list org-directory))
  '(org-directory "~/Documents/org")
  '(package-selected-packages
    (quote
-    (cloc steam shell-pop expand-region darkroom tldr which-key use-package smartparens ox-hugo org-bullets magit lua-mode flycheck exwm ess elpy doom-themes doom-modeline dashboard counsel-projectile company-quickhelp company-jedi))))
+    (org2blog pdf-tools company-nixos-options nixos-options nixpkgs-fmt pretty-sha-path nix-mode build-farm dired-single md4rd iedit steam expand-region darkroom tldr magit company-anaconda anaconda-mode racket-mode ess-R-data-view ess gpastel exwm xelb which-key use-package sudo-edit smartparens ox-hugo org-bullets god-mode flycheck doom-themes doom-modeline dashboard counsel company-quickhelp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
